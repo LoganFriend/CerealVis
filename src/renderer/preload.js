2f045d436f444d8adc56989ddb573332267dff4b
LoganFriend <loganfriend@charter.net>
2020-02-16 14:55:47 -0600

    Added in a starting point for charting data coming out of serial connection. Still needs lots of work

--- src/renderer/preload.js
+++ src/renderer/preload.js
@@ -1,25 +1,48 @@
 //Use this file as a way to preload node instructions into a window
 
 const { remote } = require('electron');
+const { chartjs } = require('chart.js');
+const { moment } = require('moment');
+const { chartjsstreaming } = require('chartjs-plugin-streaming');
 
+
+//TEST TO SHOW CONNECTION BETWEEN PRELOAD AND RENDERER
 window.closeCurrentWindow = function(){
   window.close();
 }
 
+//EXTRA
+randomScalingFactor = function() {
+	return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100);
+}
 
-serialport = require("serialport");
+
+//SERIALPORT
+//-------------------------------------------------------------------------------------------------
+const serialport = require("serialport");
 const Readline = serialport.parsers.Readline;
 
-//initialize serialport with 9600 baudrate.
-var sp = new serialport("COM8", {
-  baudRate: 9600
-});
-
-//parse incoming data line-by-line from serial port.
-const parser = sp.pipe(new Readline({ delimiter: "\r\n" }));
-parser.on("data", addText);
 
 //append incoming data to the textarea.
-function addText(event) {
+addText = function(event) {
   console.log(event);
 }
+
+addToChart = function() {
+
+}
+
+//initialize serialport with 9600 baudrate.
+connectSerial = function(port){
+  var sp = new serialport(port, {
+    baudRate: 9600
+  });
+
+  //Port is in flowing mode
+  const parser = sp.pipe(new Readline({ delimiter: "\r\n" }));
+  //parser.on("data", addText);
+  return parser
+}
+
+
+//CHARTJS
--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -9,3 +9,133 @@ closebtn.addEventListener("click", e => {
   window.closeCurrentWindow();
 });
 
+//CHART JS
+//-------------------------------------------------------------------------------------------------
+var chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
+};
+var colorNames = Object.keys(chartColors);
+
+window.onload = function() {
+	var ctx = document.getElementById('myChart').getContext('2d');
+	window.myChart = new Chart(ctx, config);
+};
+
+var color = Chart.helpers.color;
+var config = {
+	type: 'line',
+	data: {
+		datasets: [{
+			label: 'Dataset 1 (linear interpolation)',
+			backgroundColor: color(chartColors.red).alpha(0.5).rgbString(),
+			borderColor: chartColors.red,
+			fill: false,
+			lineTension: 0,
+			borderDash: [8, 4],
+			data: []
+		}, {
+			label: 'Dataset 2 (cubic interpolation)',
+			backgroundColor: color(chartColors.blue).alpha(0.5).rgbString(),
+			borderColor: chartColors.blue,
+			fill: false,
+			cubicInterpolationMode: 'monotone',
+			data: []
+		}]
+	},
+	options: {
+		title: {
+			display: true,
+			text: 'Line chart (hotizontal scroll) sample'
+		},
+		scales: {
+			xAxes: [{
+				type: 'realtime',
+				realtime: {
+					duration: 20000,
+					refresh: 1000,
+					delay: 2000,
+					onRefresh: onRefresh
+				}
+			}],
+			yAxes: [{
+				scaleLabel: {
+					display: true,
+					labelString: 'value'
+				}
+			}]
+		},
+		tooltips: {
+			mode: 'nearest',
+			intersect: false
+		},
+		hover: {
+			mode: 'nearest',
+			intersect: false
+		}
+	}
+};
+
+function onRefresh(chart, data) {
+	chart.config.data.datasets.forEach(function(dataset) {
+		dataset.data.push({
+      x: Date.now(),
+      y: data
+			//y: randomScalingFactor()
+		});
+	});
+}
+
+/*
+document.getElementById('randomizeData').addEventListener('click', function() {
+	config.data.datasets.forEach(function(dataset) {
+		dataset.data.forEach(function(dataObj) {
+			dataObj.y = randomScalingFactor();
+		});
+	});
+	window.myChart.update();
+});
+
+
+document.getElementById('addDataset').addEventListener('click', function() {
+	var colorName = colorNames[config.data.datasets.length % colorNames.length];
+	var newColor = chartColors[colorName];
+	var newDataset = {
+		label: 'Dataset ' + (config.data.datasets.length + 1),
+		backgroundColor: color(newColor).alpha(0.5).rgbString(),
+		borderColor: newColor,
+		fill: false,
+		lineTension: 0,
+		data: []
+	};
+
+	config.data.datasets.push(newDataset);
+	window.myChart.update();
+});
+
+document.getElementById('removeDataset').addEventListener('click', function() {
+	config.data.datasets.pop();
+	window.myChart.update();
+});
+
+document.getElementById('addData').addEventListener('click', function() {
+	onRefresh(window.myChart);
+	window.myChart.update();
+});
+*/
+
+//SERIALPORT
+//-------------------------------------------------------------------------------------------------
+
+addData = function(event){
+  onRefresh(window.myChart, event);
+	window.myChart.update();
+}
+
+parser = connectSerial("COM8");
+parser.on("data", addData);
