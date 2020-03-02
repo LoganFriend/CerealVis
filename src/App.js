f62f5430bf744b284c599cd15f1667277c949c99
Logan Friend <loganfriend@charter.net>
2020-03-02 17:27:57 -0600

    Removed validators

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React, { Component } from 'react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { ThemeProvider } from '@material-ui/styles';
-import validate from 'validate.js';
 
 import theme from './theme';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import './assets/scss/index.scss';
-import validators from './common/validators';
 import Routes from './Routes';
 
 const browserHistory = createBrowserHistory();
-
-validate.validators = {
-  ...validate.validators,
-  ...validators
-};
 
 export default class App extends Component {
   render() {
