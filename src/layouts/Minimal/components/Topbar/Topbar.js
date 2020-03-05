6b49c2e4cca66009466e082650bfb3622eebba05
Emilio <emilioquintana90@gmail.com>
2020-03-04 18:12:59 -0600

    Moved Navigation Items to the right

--- src/layouts/Minimal/components/Topbar/Topbar.js
+++ src/layouts/Minimal/components/Topbar/Topbar.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
-import { AppBar, Toolbar } from '@material-ui/core';
-import SettingsIcon from '@material-ui/icons/Settings';
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/styles";
+import { AppBar, Toolbar, Button, colors } from "@material-ui/core";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    boxShadow: 'none'
+    boxShadow: "none"
   },
   flexGrow: {
     flexGrow: 1
+  },
+  Button: {
+    color: colors.grey[50],
+    textTransform: "none",
+    fontWeight: theme.typography.fontWeightLight,
+    fontSize: 20
   }
 }));
 
@@ -29,18 +34,27 @@ const Topbar = props => {
     >
       <Toolbar>
         <RouterLink to="/">
-          <img
-            alt="Logo"
-            src="/images/logos/logo.svg"
-          />
+          <img alt="Logo" src="/images/logos/logo.svg" />
         </RouterLink>
-
         <div className={classes.flexGrow} />
-        
-        <RouterLink to="/settings">
-          <SettingsIcon />
+
+        <RouterLink to="/">
+          <Button className={classes.Button} activeClassName={classes.active}>
+            Home
+          </Button>
         </RouterLink>
 
+        <RouterLink to="/users">
+          <Button className={classes.Button} activeClassName={classes.active}>
+            Users
+          </Button>
+        </RouterLink>
+
+        <RouterLink to="/settings">
+          <Button className={classes.Button} activeClassName={classes.active}>
+            Settings
+          </Button>
+        </RouterLink>
       </Toolbar>
     </AppBar>
   );
