0a7ca6dd73b2b2eb2bb212a82170a5f14e78536c
Emilio <emilioquintana90@gmail.com>
2020-03-10 18:25:42 -0500

    Moved part of Serial Component to Pop up component

--- src/views/Dashboard/components/Popup/Popup.js
+++ src/views/Dashboard/components/Popup/Popup.js
@@ -1,29 +1,77 @@
 import React, { Component } from "react";
 import Popup from "reactjs-popup";
 import { Button } from "@material-ui/core";
-import { Serial } from "../../components";
 import "../Popup/style.css";
+
+function connect(e) {
+  e.preventDefault();
+  var args = new Object();
+  args.cmd = "connect";
+  args.port = "AUTO";
+  window.ipcRenderer.send("serialport", args);
+}
+
+function startstop(e) {
+  e.preventDefault();
+
+  var args = new Object();
+  args.cmd = "toggle";
+
+  if (this.state.text === "Start") {
+    this.setState({
+      text: "Stop"
+    });
+  } else {
+    this.setState({
+      text: "Start"
+    });
+  }
+
+  window.ipcRenderer.send("serialport", args);
+}
 
 class PopUp extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      open: true,
+      text: "Start"
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.connect = connect.bind(this);
+    this.startstop = startstop.bind(this);
+  }
+
+  openModal() {
+    this.setState({ open: true });
+  }
+  closeModal() {
+    this.setState({ open: false });
   }
 
   render() {
     return (
-      <Popup trigger={<Button className="button"> Open Popup </Button>} modal>
-        {close => (
+      <div>
+        <Button className="button" onClick={this.openModal}>
+          Popup
+        </Button>
+        <Popup
+          open={this.state.open}
+          closeOnDocumentClick
+          onClose={this.closeModal}
+        >
           <div className="modal">
-            <a className="close" onClick={close}>
+            <Button className="button" color="primary" onClick={this.connect}>
+              Search
+            </Button>
+            <a className="close" onClick={this.closeModal}>
               &times;
             </a>
-
-            <div className="actions">
-              <Serial />
-            </div>
           </div>
-        )}
-      </Popup>
+        </Popup>
+      </div>
     );
   }
 }
