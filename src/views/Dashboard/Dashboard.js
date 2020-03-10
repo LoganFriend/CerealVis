e024f2fa5bc492149d65ae68482e77ae3a9c741c
Logan Friend <loganfriend@charter.net>
2020-03-10 17:27:58 -0500

    merging popup

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/styles";
 import { Grid, Paper, Tabs, Tab } from "@material-ui/core";
 
-import { LineChart, Serial, Stats } from "./components";
+import { LineChart, Serial, Stats, Popup } from "./components";
 
 
 const useStyles = makeStyles(theme => ({
@@ -19,6 +19,7 @@ const Dashboard = () => {
     <Paper elevation={0} className={classes.root}>
 
       <Serial />
+      <Popup />
 
       <Grid container spacing={4}>
         <Grid item xs={2}>
