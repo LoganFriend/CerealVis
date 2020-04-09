64de65d263d065a267d13bdfe124247690bacfe2
Logan Friend <loganfriend@charter.net>
2020-04-09 09:52:31 -0500

    removed ipcMain call from serialport file

--- public/serialport.js
+++ public/serialport.js
@@ -1,6 +1,5 @@
 const SerialPort = require("serialport");
 const Readline = require("@serialport/parser-readline");
-const ipcMain = require("electron")
 
 class SerialPortClass {
   constructor() {
@@ -69,7 +68,7 @@ class SerialPortClass {
     if (valid_port) {
       this.port = new SerialPort(path, { baudRate: this.baudRate }, function (err) {
         if (err) {
-          ipcMain.send("log", "error", err.message)
+          console.log("Error: " + err.message)
         }
       });
       this.parser = this.port.pipe(new Readline({ delimiter: "\r\n" }));
